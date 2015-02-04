ca8bba9b50c7c70867a629ec95cc3132cd4f48d1
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-04 16:40:58 -0500

    @mfees @eklemen @garrettgraham appended list item upon pressing enter key only

--- js/app.js
+++ js/app.js
@@ -2,8 +2,16 @@
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
-    $(document).ready(function(){
+    $(document).ready(function(){
         
-    })
+        $('#new-todo').on('keypress', function (e) { 
+            if(e.which == 13) {
+        var newItem = '<li><div class="view"><input class="toggle" type="checkbox"><label>Buy a unicorn</label><button class="destroy"></button></div><input class="edit" value="Rule the web"></li>';
+            }
+        $('#todo-list').append(newItem);
+    
+        });
+    });
 
 })(window);
+
