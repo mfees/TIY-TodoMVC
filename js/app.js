0eb18cfbce00f11d0e92f93a81e9f203f90d1b69
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-04 19:29:02 -0500

    @mfees @eklemen @garrettgraham @jrutishauser trying to make foother hide with empty task list

--- js/app.js
+++ js/app.js
@@ -9,11 +9,24 @@
                 console.log(newTask);
                 var newItem = '<li><div class="view"><input class="toggle" type="checkbox"><label>' + newTask + '</label><button class="destroy"></button></div><input class="edit" value="'+ newTask +'"></li>';
                         $('#todo-list').append(newItem);
+                
+        var listLength = $('#todo-list li').length;    
+        if ( listLength  == 0){
+        return $('#footer').addClass('hidden');
+    } else {
+        return $('#footer').removeClass('hidden');
+            }
 
             }
-    
+            
         });
+        
+
+    
     });
 
+    
+    
+    
 })(window);
 
