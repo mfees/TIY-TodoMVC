14ecb03ad2f36c20fff43fd3969547534ea51d1e
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-04 20:53:59 -0500

    @eklemen @jrutishauser @mfees @garrettgraham Counter works and delete works

--- js/app.js
+++ js/app.js
@@ -9,21 +9,36 @@
                 console.log(newTask);
                 var newItem = '<li><div class="view"><input class="toggle" type="checkbox"><label>' + newTask + '</label><button class="destroy"></button></div><input class="edit" value="'+ newTask +'"></li>';
                         $('#todo-list').append(newItem);
-                
-        var listLength = $('#todo-list li').length;    
-        if ( listLength  == 0){
-        return $('#footer').addClass('hidden');
-    } else {
-        return $('#footer').removeClass('hidden');
-            }
-
-            }
+                    $('#new-todo').val('');
+                    var lengthOfToDo = $('#todo-list li').length;
+                    $('#todo-count strong').text(lengthOfToDo);
+//        var listLength = $('#todo-list li').length;    
+//        if ( listLength  >= 1){
+//        $('#footer').removeClass('hidden');
+//    } else {
+//        $('#footer').addClass('hidden');
+//
+//            }
+   
+            } // if loop closer, hey girl, heyyyyy aka you need me bro!
+        $(".destroy").on('click', function() {
+        $(this).parent().parent().remove();
+        var lengthOfToDo = $('#todo-list li').length;
+        $('#todo-count strong').text(lengthOfToDo);
+        }); 
             
         });
+//      var toggleAll = $('#toggle-all');
+//        toggleAll.on('click', function(e){
+//        $('.toggle').attr('checked', true);
+//        $('li').toggleClass('completed');
         
 
+        
+
+    });  
+
     
-    });
 
     
     
