c19c2a1edc05f535e593430478b7625e3eec642d
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-21 12:21:01 -0500

    setup the array of objects in the app.js file for tasks. setup so that items can be checked off in the list. tasks in the array are added to the list however I can not push to the array yet from the html file

--- js/app.js
+++ js/app.js
@@ -1,50 +1,23 @@
 (function (window) {
-	'use strict';
-
-	// Your starting point. Enjoy the ride!
-    $(document).ready(function(){
-        $('#new-todo').on('keypress', function (e) { 
-            if(e.which == 13) {
-                var newTask = $('#new-todo').val();
-                console.log(newTask);
-                var newItem = '<li><div class="view"><input class="toggle" type="checkbox"><label>' + newTask + '</label><button class="destroy"></button></div><input class="edit" value="'+ newTask +'"></li>';
-                        $('#todo-list').append(newItem);
-                    $('#new-todo').val('');
-                    var lengthOfToDo = $('#todo-list li').length;
-                    $('#todo-count strong').text(lengthOfToDo);
-//        var listLength = $('#todo-list li').length;    
-//        if ( listLength  >= 1){
-//        $('#footer').removeClass('hidden');
-//    } else {
-//        $('#footer').addClass('hidden');
-//
-//            } // an attempt at hiding and showing the foot ver 5.0 that was a blvd of broken dreams
-   
-            } // if loop closer, hey girl, heyyyyy aka you need me bro!
-        $(".destroy").on('click', function() {
-        $(this).parent().parent().remove();
-        var lengthOfToDo = $('#todo-list li').length;
-        $('#todo-count strong').text(lengthOfToDo);
-        
-//        if ($(".toggle").attr('checked', true)) {
-//            $('li').addClass('completed');
-//        };   // an attempt at getting the checkboxes to apply the completed task
-        
-        }); 
-            
-        });
-//      var toggleAll = $('#toggle-all');
-//        toggleAll.on('click', function(e){
-//        $('.toggle').attr('checked', true);
-//        $('li').toggleClass('completed');
-        
-
-        
-
-    });  
 
     
+var app = angular.module('Todomvc', [ ]);
 
+app.controller('TaskController', function(){
+   
+    this.items = tasks;
+    this.addItem = function(){
+	        this.items.push({text: this.todotext});
+	        console.log("codebone");
+	    };
+});               
+
+
+var tasks = [
+    { text: "learn angular"},
+    { text: "buy a pony" },
+    { text: "another engaging task" }
+];
     
     
     
