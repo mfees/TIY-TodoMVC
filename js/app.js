be80a6f0c4836feb7dea48d0b539e9d8bb31fb52
Maddie Fees <madison.fees90@gmail.com>
2015-02-21 12:33:24 -0700

    getting caught up with group

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
-(function () {
-    var app = angular.module('TDMVC', [ ]);
-        app.controller('TodoController', functiion (){
-        
-        this.tasks =
-        [{text: 'Buy A Unicorn'}, {text: 'Buy A Unicorn'}];
-        });
-}); ();
+(function (window) {
+
+    
+var app = angular.module('Todomvc', [ ]);
+
+app.controller('TaskController', function(){
+   
+    this.items = tasks;
+    this.addItem = function(){
+	        this.items.push({text: this.todotext, done:false});
+	        console.log("codebone");
+	    };
+});               
+
+
+var tasks = [
+    { text: "learn angular"},
+    { text: "buy a pony" },
+    { text: "another engaging task" },
+    { text: "getting things done" }
+];
+    
+    
+    
+})(window);
