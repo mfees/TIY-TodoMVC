41e82c3a3100d6d8e94f595aadf1d5a4ffcbe719
Evan Klemen <ejklemen@gmail.com>
2015-02-21 22:10:32 -0500

    deleted stupid .swo and swp cause jon doenst know how to git ignore

--- js/app.js
+++ js/app.js
@@ -1,22 +1,28 @@
 (function (window) {
-
-    
 var app = angular.module('TodoMVC', [ ]);
 
 app.controller('TodoController', function(){
    
     this.items = tasks;
     this.addItem = function(newItem){
-	        this.items.push({text: this.newItem});
+	        this.items.push({text: this.newItem, done: false});
             this.newItem = '';
 	    };
+    
+    this.clear = function(){
+      var oldTodos = this.items;
+        this.items = [];
+        angular.forEach(oldTodos, function(item){
+            if (!item.done) this.items.push(item);
+        });
+    };
 });               
 
 
 var tasks = [
-    { text: "learn angular", checked: true},
-    { text: "buy a pony", checked: false},
-    { text: "another engaging task", checked: false}
+    { text: "learn angular", done: true},
+    { text: "buy a pony", done: false},
+    { text: "another engaging task", done: false}
 ];
         
 })(window);
