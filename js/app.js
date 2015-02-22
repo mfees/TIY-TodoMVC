689ee99c6a13777ffebf787e4ac591d99e4c95ba
Evan Klemen <ejklemen@gmail.com>
2015-02-21 23:37:17 -0500

    working on item counter updating and deleting items from list

--- js/app.js
+++ js/app.js
@@ -9,19 +9,27 @@ app.controller('TodoController', function(){
             this.newItem = '';
 	    };
     
-    this.clear = function(){
-      var oldTodos = this.items;
-        this.items = [];
-        angular.forEach(oldTodos, function(item){
-            if (!item.done) this.items.push(item);
-        });
+    this.remove = function(item){
+        this.items.splice(this.items.indexOf(item), 1);
     };
+    
+    this.itemCount = function(item){
+        if(!item.done) this.items.length;
+    }
+    
+//    this.clear = function(){
+//      var oldTodos = this.items;
+//        this.items = [];
+//        angular.forEach(this.items, function(item){
+//            if (!item.done) this.items.push(item);
+//        });
+//    }; // supposed to clear all items that are marked done
 });               
 
 
 var tasks = [
     { text: "learn angular", done: true},
-    { text: "buy a pony", done: false},
+    { text: "buy a pony", done: true},
     { text: "another engaging task", done: false}
 ];
         
