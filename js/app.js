dd037298495fe3ca1070a0f1a2327b518b5be36e
Maddie Fees <madison.fees90@gmail.com>
2015-02-21 20:32:35 -0700

    playing around, trying to add the delete functionality, didn't really work out

--- js/app.js
+++ js/app.js
@@ -8,16 +8,15 @@ app.controller('TodoController', function(){
     this.items = tasks;
     var newItem;
     this.addItem = function(newItem){
-	        this.items.push({text: this.newItem});
-            this.newItem = '';
-	    };
+        this.items.push({text: this.newItem});
+        this.newItem = '';
+    };
 });               
 
-
 var tasks = [
-    { text: "learn angular"},
-    { text: "buy a pony" },
-    { text: "another engaging task" }
+    { text: "learn angular", checked: true},
+    { text: "buy a pony", checked: false},
+    { text: "another engaging task", checked:false}
 ];
         
 })(window);
